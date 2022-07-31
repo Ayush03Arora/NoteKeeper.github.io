302e7dac39c59a9b687bd3e69be008ad1a93f638
Ayush <aayusharora4444@gmail.com>
2022-07-31 23:59:50 +0530

    Deploy

---
+++ build/precache-manifest.76d5970590617805308e23c1a9c6fae0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "431cb630bc91105a80386de9adf87e64",
+    "url": "/NoteKeeper.github.io/index.html"
+  },
+  {
+    "revision": "aed5558a41a5c248a185",
+    "url": "/NoteKeeper.github.io/static/js/2.1ec8088d.chunk.js"
+  },
+  {
+    "revision": "b45aa87242fefc675c38",
+    "url": "/NoteKeeper.github.io/static/js/main.b379a50f.chunk.js"
+  },
+  {
+    "revision": "533a14c899d83041cefe",
+    "url": "/NoteKeeper.github.io/static/js/runtime-main.d4800cc1.js"
+  }
+]);
